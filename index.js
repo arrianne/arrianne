f54a59406c364ff10d4c170f36c5347f704912b5
arrianne <arrianneoshea@hotmail.co.uk>
2020-05-23 15:03:44 +1000

    adding burger menu and styling mode toggler

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 $(document).ready(function() {
-console.log("i'm working");
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+  console.log("i'm working");
+  const toggleSwitch = document.querySelector('.toggle input[type="checkbox"]');
+  const currentTheme = localStorage.getItem('theme');
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme) {
+      document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}
+      if (currentTheme === 'dark') {
+          toggleSwitch.checked = true;
+      }
+  }
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }
-}
+  function switchTheme(e) {
+      if (e.target.checked) {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
+      }
+      else {        document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+      }
+  }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+  // Adding background to burger menu on scroll
+
+    $(window).scroll(function(){
+      	var scroll = $(window).scrollTop();
+    	  if (scroll > 300) {
+    	    $(".burger-menu_container").css("background" , "#36c9c6");
+          $(".bar").css("background" , "white");
+    	  }
+    	  else{
+    		  $(".burger-menu_container").css("background" , "none");
+          $(".bar").css("background" , "#34435e");
+    	  }
+      })
+
+    // should start at 0
+
+    $('.burger-menu').click(function(){
+      $(this).toggleClass('active');
+      $(".header").toggleClass("header-active");
+      return false;
+    });
+
+  $('.title-box').scroll(function() {
+      $(this).css({
+        'width' : '100%',
+        'transition' : '0.4s'
+      });
+    });
+
+
+
+
+
 });
