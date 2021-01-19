c6ca0e301da8bed6216cd8ec2c4c21cf9bc4063f
arrianne <arrianneoshea@hotmail.co.uk>
2021-01-19 16:52:51 +1000

    adding fancy button hover affects

--- contact.php
+++ contact.php
@@ -1,9 +1,11 @@
+<!-- https://www.youtube.com/watch?v=j7lnU8hJRjo -->
+<!-- this was very helpful ^ -->
 
 <?php // Check if form was submitted
 if(isset($_POST['submit'])) {
   $name = trim(htmlspecialchars($_POST['name'], ENT_QUOTES));
   $email = trim($_POST['email']);
-$messsage = trim(htmlspecialchars($_POST['message'], ENT_QUOTES));
+  $messsage = trim(htmlspecialchars($_POST['message'], ENT_QUOTES));
 
   $mailTo = "arrianneoshea@hotmail.co.uk";
   $headers = "From: ".$email;
