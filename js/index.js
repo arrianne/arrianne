631136283db39aff9292ceb9dfc2ab257f53929d
arrianne <arrianneoshea@hotmail.co.uk>
2021-01-22 09:39:50 +1000

    using ajax for form submission

--- js/index.js
+++ js/index.js
@@ -127,5 +127,41 @@ $(document).ready(function () {
           }
       });
 
+      /// Form submission to formspree with ajax
+
+      $('#contact-form').on('submit', function(e) {
+    		e.preventDefault();
+
+    		//get the name field value
+    		var name = $('#name').val();
+    		//get the name field value
+    		var email = $('#email').val();
+    		//get the comments
+    		var comments = $('#comments').val();
+
+    		//pretend we don't need validation
+
+    		//send to formspree
+    		$.ajax({
+    			url:'https://formspree.io/arrianneoshea@hotmail.co.uk',
+    			method:'POST',
+    			data:{
+    				name:name,
+    				_replyto:email,
+    				 email:email,
+    				comments:comments,
+    				_subject:'My Form Submission',
+    			},
+    			dataType:"json",
+    			success:function() {
+    				console.log('success');
+    				$('#formBlock').hide();
+    				$('#thankyouBlock').show();
+    			}
+
+    		});
+
+    	});
+
 
 });
